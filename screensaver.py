594b8141460851835cb2480f145e9a3cc63b86d1
Office <garretbs@gmail.com>
2018-06-29 15:17:12 -0400

    Trying to separate physics from frame rate

--- screensaver.py
+++ screensaver.py
@@ -12,38 +12,51 @@ def main():
 	bg.fill((0, 0, 0))
 	
 	bsize = 16
-	sx = sy = 4
-	sy = 0
+	sx = sy = 4.0
+	#sy = 0
 	x = y = 48
-	#box = pygame.Rect(48, 48, bsize, bsize)
+	#r = pygame.Rect(0, 0, bsize, bsize)
+	box = pygame.Surface((bsize, bsize))
 	fcolor = (255, 255, 255)
+	box.fill(fcolor)
 	
 	fps = 60.0
 	frametime = int(1000.0/fps) #in milliseconds
+	time_since_update = 0.0
+	tick = fps/1000.0
+	clock = pygame.time.Clock()
 	
+	sx *= fps
+	sy *= fps
+	
+	#todo: more advanced. separate physics from frame rate. find better way to limit frame rate. interpolate between frames
 	while True:
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
 			elif event.type == MOUSEBUTTONDOWN:
-					print('called')
+					#print('called')
+					x = 48
+					y = 48
 					#box.move(-box.x, -box.y)
 					pygame.event.clear() #don't want queued clicks building up			
 		
-		#box = box.move(sx, sy)
-		#print(box.y)
-		#print(box.y + bsize >= size[1])
+		time_since_update = (clock.tick(fps))/1000.0
+		x += sx * time_since_update
+		y += sy * time_since_update
+		#box = box.move(sx, sy)		
 		
-		x += sx
-		y += sy
+		#print(time_since_update)
 		
 		if x + bsize >= size[0] or x <= 0:
-			sy *= -1
-		elif y + bsize >= size[1] or y <= 0:
 			sx *= -1
+		if y + bsize >= size[1] or y <= 0:
+			sy *= -1
+		
 		screen.blit(bg, (0,0))
+		screen.blit(box, (x, y))
 		#pygame.draw.rect(screen, fcolor, box)
 		pygame.display.flip()
-		pygame.time.delay(frametime)
+		#pygame.time.delay(frametime)
 
 if __name__ == '__main__': main()
